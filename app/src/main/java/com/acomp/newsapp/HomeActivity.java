82350f1768902179c26d58f63396b86f3f26e1fc
mramirid <amir.rhythm@gmail.com>
2019-10-10 21:24:58 +0700

    Custom Title font

--- app/src/main/java/com/acomp/newsapp/HomeActivity.java
+++ app/src/main/java/com/acomp/newsapp/HomeActivity.java
@@ -1,9 +1,12 @@
 package com.acomp.newsapp;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.res.ResourcesCompat;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,8 +14,5 @@ public class HomeActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		Toolbar toolbar = findViewById(R.id.toolbar);
-		toolbar.setTitle(getString(R.string.app_name));
 	}
 }
