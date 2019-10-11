e9061da363e6e2856452d278851047a9cc4f373e
mramirid <amir.rhythm@gmail.com>
2019-10-11 16:05:12 +0700

    Added recyclerview

--- app/src/main/java/com/acomp/khobarapp/ui/home/HomeActivity.java
+++ app/src/main/java/com/acomp/khobarapp/ui/home/HomeActivity.java
@@ -1,16 +1,69 @@
 package com.acomp.khobarapp.ui.home;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.acomp.khobarapp.R;
+import com.acomp.khobarapp.adapter.HeadlineNewsAdapter;
+import com.acomp.khobarapp.adapter.RegularNewsAdapter;
+import com.acomp.khobarapp.viewmodel.ViewModelFactory;
 
 public class HomeActivity extends AppCompatActivity {
+
+	private RecyclerView rvHeadlineNews, rvRegularNews;
+	private ProgressBar progressBar;
+
+	private int recyclerViewsLoadedCount = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+
+		rvHeadlineNews = findViewById(R.id.rv_headline);
+		rvRegularNews = findViewById(R.id.rv_regular);
+		progressBar = findViewById(R.id.progress_bar);
+		progressBar.setVisibility(View.VISIBLE);
+
+		HeadlineNewsAdapter headlineNewsAdapter = new HeadlineNewsAdapter(this);
+		RegularNewsAdapter regularNewsAdapter = new RegularNewsAdapter(this);
+		HomeViewModel homeViewModel = obtainViewModel(this);
+
+		homeViewModel.getHeadlineNews().observe(this, newsEntities -> {
+			onNewsLoaded();
+			headlineNewsAdapter.setHeadlineNews(newsEntities);
+			headlineNewsAdapter.notifyDataSetChanged();
+		});
+
+		homeViewModel.getRegularNews().observe(this, newsEntities -> {
+			onNewsLoaded();
+			regularNewsAdapter.setRegularNews(newsEntities);
+			regularNewsAdapter.notifyDataSetChanged();
+		});
+
+		rvHeadlineNews.setLayoutManager(new LinearLayoutManager(getBaseContext(), LinearLayoutManager.HORIZONTAL, false));
+		rvHeadlineNews.setHasFixedSize(true);
+		rvHeadlineNews.setAdapter(headlineNewsAdapter);
+
+		rvRegularNews.setLayoutManager(new LinearLayoutManager(getBaseContext()));
+		rvRegularNews.setHasFixedSize(true);
+		rvRegularNews.setAdapter(regularNewsAdapter);
+	}
+
+	private HomeViewModel obtainViewModel(AppCompatActivity activity) {
+		ViewModelFactory factory = ViewModelFactory.getInstance();
+		return ViewModelProviders.of(activity, factory).get(HomeViewModel.class);
+	}
+
+	private void onNewsLoaded() {
+		// Progress bar akan dihilangkan dari layar apabila kedua recyclerview telah diload
+		if (++recyclerViewsLoadedCount == 2)
+			progressBar.setVisibility(View.GONE);
 	}
 }
