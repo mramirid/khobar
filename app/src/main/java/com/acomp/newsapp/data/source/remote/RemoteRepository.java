af9cb4aab5c5bcaef6f1cf717e5b06b04b850da5
mramirid <amir.rhythm@gmail.com>
2019-10-11 10:49:11 +0700

    Add RemoteRepository class

--- app/src/main/java/com/acomp/newsapp/data/source/remote/RemoteRepository.java
+++ app/src/main/java/com/acomp/newsapp/data/source/remote/RemoteRepository.java
@@ -1,6 +1,35 @@
 package com.acomp.newsapp.data.source.remote;
 
+import com.acomp.newsapp.data.source.remote.response.NewsResponse;
+import com.acomp.newsapp.utils.JsonHelper;
+
+import java.util.ArrayList;
+
 public class RemoteRepository {
 
 	private static RemoteRepository INSTANCE;
+	private JsonHelper jsonHelper;
+
+	private RemoteRepository(JsonHelper jsonHelper) {
+		this.jsonHelper = jsonHelper;
+	}
+
+	public static RemoteRepository getInstance(JsonHelper jsonHelper) {
+		if (INSTANCE == null)
+			INSTANCE = new RemoteRepository(jsonHelper);
+		return INSTANCE;
+	}
+
+	public void getHeadlineNews(LoadNewsCallback callback) {
+		jsonHelper.loadHeadlineNews(callback);
+	}
+
+	public void getRegularNews(LoadNewsCallback callback) {
+		jsonHelper.loadRegularNews(callback);
+	}
+
+	public interface LoadNewsCallback {
+		void onNewsReceived(ArrayList<NewsResponse> newsResponses);
+		void onDataNotAvailable();
+	}
 }
