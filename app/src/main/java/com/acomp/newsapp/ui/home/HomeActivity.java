e4779184cd376d10d9e391051cc2cb322fe1de6f
mramirid <amir.rhythm@gmail.com>
2019-10-11 14:15:50 +0700

    Add progress bar

--- app/src/main/java/com/acomp/newsapp/ui/home/HomeActivity.java
+++ app/src/main/java/com/acomp/newsapp/ui/home/HomeActivity.java
@@ -1,12 +1,8 @@
 package com.acomp.newsapp.ui.home;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.content.res.ResourcesCompat;
 
 import com.acomp.newsapp.R;
 
@@ -15,6 +11,6 @@ public class HomeActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_home);
 	}
 }
