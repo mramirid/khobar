00931a71de452574d5e7f8fd298f7c53309368bf
mramirid <amir.rhythm@gmail.com>
2019-10-11 11:01:28 +0700

    Add NewsDataSource interface

--- app/src/main/java/com/acomp/newsapp/data/source/NewsDataSource.java
+++ app/src/main/java/com/acomp/newsapp/data/source/NewsDataSource.java
@@ -1,4 +1,13 @@
 package com.acomp.newsapp.data.source;
 
+import androidx.lifecycle.LiveData;
+
+import com.acomp.newsapp.data.entity.NewsEntity;
+
+import java.util.ArrayList;
+
 public interface NewsDataSource {
+	LiveData<ArrayList<NewsEntity>> getHeadlineNews();
+	LiveData<ArrayList<NewsEntity>> getRegularNews();
+	LiveData<NewsEntity> getNews(String newsType, String url);
 }
