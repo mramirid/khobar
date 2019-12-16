a5f564282c1708979fc7bb3d10ff2d784ea1a92b
Sunu Ilham Pradika <sunudika@gmail.com>
2019-12-16 20:20:24 +0700

    added code to implement pressed back twice to exit

--- app/src/main/java/com/acomp/khobarapp/ui/home/HomeActivity.java
+++ app/src/main/java/com/acomp/khobarapp/ui/home/HomeActivity.java
@@ -1,10 +1,12 @@
 package com.acomp.khobarapp.ui.home;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,12 +16,36 @@ import com.acomp.khobarapp.adapter.HeadlineNewsAdapter;
 import com.acomp.khobarapp.adapter.RegularNewsAdapter;
 import com.acomp.khobarapp.utils.SpacesItemDecoration;
 import com.acomp.khobarapp.viewmodel.ViewModelFactory;
+import com.google.android.material.snackbar.Snackbar;
 
 public class HomeActivity extends AppCompatActivity {
 
 	private ProgressBar progressBar;
 
 	private int recyclerViewsLoadedCount = 0;
+
+	private boolean doubleBackPressed = false;
+
+	@Override
+	public void onBackPressed() {
+
+		if(doubleBackPressed) {
+			super.onBackPressed();
+			return;
+		} else {
+			doubleBackPressed = true;
+			final ConstraintLayout constraintLayout = findViewById(R.id.acivity_home);
+			Snackbar.make(constraintLayout, getString(R.string.tekan_lagi), Snackbar.LENGTH_SHORT).show();
+
+			new Handler().postDelayed(new Runnable() {
+
+				@Override
+				public void run() {
+					doubleBackPressed = false;
+				}
+			}, 2000);
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
